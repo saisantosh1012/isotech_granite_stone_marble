8cf4f21b999f607176919e788b1ac7e6f9a9a7be
D SAI SANTOSH <103347778+saisantosh1012@users.noreply.github.com>
2025-09-04 16:42:40 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,46 @@
+import type {NextConfig} from 'next';
+import {config} from 'dotenv';
+
+config(); // Load environment variables from .env file
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/videos/',
+          outputPath: 'static/videos/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
